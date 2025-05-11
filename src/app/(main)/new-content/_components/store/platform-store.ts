2098506f02551babc8debdb118ec65bc1b07ea3a
dvp-tae <th3016@naver.com>
2025-05-11 18:02:03 +0900

    feat: 메인 플랫폼 선택 스탭 구현

---
+++ src/app/(main)/new-content/_components/store/platform-store.ts
@@ -0,0 +1,25 @@
+import { Category, Platforms } from "@/types/platform";
+import { create } from "zustand";
+
+interface PlatformState {
+  selectedPlatform: Platforms | null;
+  selectedCategory: Category | null;
+  setSelectedPlatform: (platform: Platforms) => void;
+  setSelectedCategory: (category: Category) => void;
+}
+
+export const usePlatformStore = create<PlatformState>((set, get) => ({
+  selectedPlatform: null,
+  selectedCategory: null,
+  setSelectedPlatform: (platform: Platforms) => {
+    const prevPlatform = get().selectedPlatform;
+    if (prevPlatform === platform) {
+      set({ selectedPlatform: null, selectedCategory: null });
+    } else {
+      set({ selectedPlatform: platform, selectedCategory: Category.POST });
+    }
+  },
+  setSelectedCategory: (category: Category) => {
+    set({ selectedCategory: category });
+  },
+}));
