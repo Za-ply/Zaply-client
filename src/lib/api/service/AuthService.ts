b02457c893183b8cbd1ab08f8cee2efe90c87c9a
dvp-tae <th3016@naver.com>
2025-05-22 10:04:21 +0900

    fix: 리프래쉬 토큰 로직 수정

--- src/lib/api/service/AuthService.ts
+++ src/lib/api/service/AuthService.ts
@@ -19,7 +19,8 @@ const authService = {
     try {
       const response = await authController.logout();
 
-      tokenManager.removeTokens();
+      // service 객체에서 토큰 삭제 처리가 아닌 logout 훅에서 onSuccess, onError 처리하도록 변경
+      // tokenManager.removeTokens();
       return response;
     } catch (error) {
       console.error("Logout failed:", error);
@@ -43,18 +44,6 @@ const authService = {
       return response.data;
     } catch (error) {
       console.error("Email duplicate check failed:", error);
-      throw new Error(error as string);
-    }
-  },
-
-  refreshToken: async (): Promise<ApiResponse<LoginData>> => {
-    try {
-      const response = await authController.refreshToken();
-
-      tokenManager.setTokens(response.data.accessToken, response.data.refreshToken);
-      return response;
-    } catch (error) {
-      console.error("Token refresh failed:", error);
       throw new Error(error as string);
     }
   },
