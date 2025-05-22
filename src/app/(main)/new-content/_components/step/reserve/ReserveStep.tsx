e4dd7fd44757abd32430e968af7e277dc0f0e566
youth-o <127082479+youth-o@users.noreply.github.com>
2025-05-23 01:29:01 +0900

    feat: 로딩 모달 구현

--- src/app/(main)/new-content/_components/step/reserve/ReserveStep.tsx
+++ src/app/(main)/new-content/_components/step/reserve/ReserveStep.tsx
@@ -1,12 +1,30 @@
+"use client";
+
 import { Button, CheckIcon } from "@/components";
 import { SelectUploadType } from "./SelectUploadType";
+import { useRouter } from "next/navigation";
 
 const ReserveStep = () => {
+  const router = useRouter();
+
+  const handleComplete = () => {
+    // /loading?modal=true로 push (병렬라우팅)
+    router.push("/loading?modal=true");
+
+    setTimeout(() => {
+      // 모달 닫기 (뒤로가기)
+      router.back();
+    }, 3000);
+  };
+
   return (
     <>
       <SelectUploadType />
       <div className="fixed bottom-[60px] left-1/2 -translate-x-1/2 w-full max-w-[440px] px-5 z-50">
-        <Button className="w-full text-button1 text-grayscale-100" leftIcon={<CheckIcon />}>
+        <Button
+          className="w-full text-button1 text-grayscale-100"
+          leftIcon={<CheckIcon />}
+          onClick={handleComplete}>
           완료
         </Button>
       </div>
