a492bbe3ea040bb7e644064cba26737c87460a6e
dvp-tae <th3016@naver.com>
2025-05-26 22:07:33 +0900

    feat: 게시물 불러오기 기능 구현

--- src/app/(main)/[projectId]/new-content/_components/step/content-make/PlatfomButton.tsx
+++ src/app/(main)/[projectId]/new-content/_components/step/content-make/PlatfomButton.tsx
@@ -8,6 +8,7 @@ import { Platforms } from "@/types/platform";
 import { platformConfig } from "../../config/platform-config";
 import { useContentMakeStore } from "../../store/content-make-store";
 import { useProfileImage } from "../../hooks/useProfileImage";
+import { cn } from "@/utils";
 
 interface PlatformButtonProps {
   isAccountConnected: boolean;
@@ -16,6 +17,7 @@ interface PlatformButtonProps {
   type: "upload" | "content";
   isDisabled?: boolean;
   isFirst?: boolean;
+  className?: string;
 }
 
 const PlatformButton = ({
@@ -25,6 +27,7 @@ const PlatformButton = ({
   type,
   isDisabled = false,
   isFirst = false,
+  className,
 }: PlatformButtonProps) => {
   const [isChecked, setIsChecked] = useState(false);
   const { postData, selectedContentPlatform, setUploadPlatforms, setSelectedContentPlatform } =
@@ -35,11 +38,12 @@ const PlatformButton = ({
     if (type === "content" && isFirst) {
       setIsChecked(true);
     }
-  }, []);
+  }, [type, isFirst]);
 
   useEffect(() => {
     if (type === "upload") {
       setIsChecked(postData.uploadPlatforms.includes(platform));
+      setUploadPlatforms(postData.uploadPlatforms);
     } else if (type === "content") {
       setIsChecked(selectedContentPlatform === platform);
     }
@@ -64,7 +68,7 @@ const PlatformButton = ({
   };
 
   return (
-    <button type="button" disabled={isDisabled} className="relative w-12 h-12">
+    <button type="button" disabled={isDisabled} className={cn("relative w-12 h-12", className)}>
       <div className="flex flex-col items-center justify-center gap-2">
         <div
           onClick={handleClick}
