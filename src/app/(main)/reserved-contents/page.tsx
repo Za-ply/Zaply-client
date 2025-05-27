4eeab271a3ba55d36f0db70d78ff98d4276626c1
youth-o <127082479+youth-o@users.noreply.github.com>
2025-05-27 16:25:00 +0900

    fix: 라우터 이동 수정

--- src/app/(main)/reserved-contents/page.tsx
+++ src/app/(main)/reserved-contents/page.tsx
@@ -12,7 +12,13 @@ export default function ReservedContentsPage() {
     <div>
       <Container className="overflow-y-scroll scrollbar-hide bg-b300-g100 flex flex-col gap-[28px] pb-[185px] min-h-real-screen">
         <TopBar
-          left={<ArrowIcon type="left" className="cursor-pointer" onClick={() => router.back()} />}
+          left={
+            <ArrowIcon
+              type="left"
+              className="cursor-pointer"
+              onClick={() => router.push("/main")}
+            />
+          }
           center={<p className="text-t4 text-grayscale-900">예약된 콘텐츠</p>}
         />
         <div className="flex flex-col">
